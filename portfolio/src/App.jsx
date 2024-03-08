b209cddaafb3352ee0d4a264c228c6b82e19bbf7
salimxfardeheb <salimfardeheb442@gmail.com>
2024-03-08 15:15:30 +0100

    creating card component for my advantage section

--- portfolio/src/App.jsx
+++ portfolio/src/App.jsx
@@ -2,6 +2,16 @@ import React from "react";
 import NavbarDesktop from "./navbar/navbarDesktop";
 import NavbarMobile from "./navbar/navbarMobile";
 import HeroSection from "./hero-section/heroSection";
+import Card from "./cards/cards";
+import Professional from "./images/rating-cards/Professional.png"
+import Fast from "./images/rating-cards/Fast.png"
+import Communication from "./images/rating-cards/Communications.png"
+
+
+const professionalDescription = "A seasoned professional in my field, I bring expertise and dedication to ensure top-tier work."
+const FastDescription = "Swift and efficient, I prioritize timely project delivery without compromising quality."
+const CommunicationDescription = "Adept at clear and open communication, I foster collaboration for successful project outcomes."
+
 
 const App = () => {
   return (
@@ -10,6 +20,11 @@ const App = () => {
         <NavbarDesktop />
         <NavbarMobile />
         <HeroSection />
+        <div className="flex flex-col md:flex-row mx-[12%] gap-12 absolute top-[90%]">
+        <Card icon={Professional} title="Professional" description={professionalDescription}/>
+        <Card icon={Fast} title="Fast" description={FastDescription}/>
+        <Card icon={Communication} title="Communication" description={CommunicationDescription}/>
+        </div>
       </div>
     </>
   );
---
+++ portfolio/src/cards/cards.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+import { string } from "prop-types";
+
+
+
+const Card = ({ icon, title, description }) => {
+  return (
+    <div className="bg-white md:w-1/3 flex flex-col items-center gap-4 px-5 pt-4 pb-7 rounded-2xl shadow-xl">
+      <div>
+        <img src={icon} alt={`${title} icon`} />{" "}
+      </div>
+      <div>
+        <p className=" text-MobileHeader4 lg:text-Header4">{title}</p>
+      </div>
+      <div>
+        <p className="text-MobileHeader5 lg:text-Header5 text-nevada text-center">
+          {description}
+        </p>
+      </div>
+    </div>
+  );
+};
+
+Card.defaultProps = {
+    icon: string,
+    title: string,
+    description: string,
+  };
+
+export default Card;
